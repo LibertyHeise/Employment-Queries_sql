2f117c350d16ffb69150ddee498c0beaeec66d1e
LibertyHeise <109040678+LibertyHeise@users.noreply.github.com>
2022-11-21 15:29:46 -0800

    Updates to table creation

--- EmployeeSQL/tables_employees.sql
+++ EmployeeSQL/tables_employees.sql
@@ -1,38 +1,46 @@
-
 -- EmployeeSQL table creations
 
 CREATE TABLE departments (
-dept_no VARCHAR (30),
-dept_name (VARCHAR) (30)
+dept_no VARCHAR (10),
+dept_name (VARCHAR) (30),
+PRIMARY KEY(dept_no)
 );
 
 CREATE TABLE dept_emp (
-  emp_no VARCHAR (10), 
-	dept_no VARCHAR(10)
-	);
+emp_no VARCHAR (10), 
+dept_no VARCHAR(10),
+FOREIGN KEY(emp_no) REFERENCES employees (emp_no), 
+FOREIGN KEY(dept_no) REFERENCES departments (dept_no),
+PRIMARY KEY (emp_no, dept_no)
+);
 	
 CREATE TABLE dept_manager (
-  dept_no VARCHAR (10), 
-	emp_no VARCHAR(10)
-	);	
+dept_no VARCHAR (10), 
+emp_no VARCHAR(10),
+FOREIGN KEY(emp_no) REFERENCES employees (emp_no), 
+FOREIGN KEY(dept_no) REFERENCES departments (dept_no),
+PRIMARY KEY (emp_no, dept_no)
+);	
 	
 CREATE TABLE employees (
-  emp_no VARCHAR(10), 
-	emp_title_id VARCHAR(30), 
-	birth_date VARCHAR(10), 
-	first_name VARCHAR(30), 
-	last_name VARCHAR(30), 
-	sex VARCHAR(10), 
-	hire_date VARCHAR(10)
-	);	
+emp_no VARCHAR(10), 
+emp_title_id VARCHAR(30), 
+birth_date VARCHAR(10), 
+first_name VARCHAR(30), 
+last_name VARCHAR(30), 
+sex VARCHAR(10), 
+hire_date VARCHAR(10)
+PRIMARY KEY (emp_no)
+);	
 
 CREATE TABLE salaries (
-  emp_no VARCHAR(10), 
-	salary INT
-	);	
+emp_no VARCHAR(10), 
+salary INT,
+PRIMARY KEY (emp_no)
+);	
 	
 CREATE TABLE titles (
-  title_id VARCHAR (10), 
-	title VARCHAR(30)
-	);	
-	
+title_id VARCHAR (10), 
+title VARCHAR(30),
+PRIMARY KEY (title_id)
+);		

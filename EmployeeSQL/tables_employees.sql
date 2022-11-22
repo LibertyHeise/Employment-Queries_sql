7e4bd4cecfc55eeb355802b550336d178376a0ba
LibertyHeise <109040678+LibertyHeise@users.noreply.github.com>
2022-11-21 16:37:44 -0800

    updates

--- EmployeeSQL/tables_employees.sql
+++ EmployeeSQL/tables_employees.sql
@@ -7,7 +7,7 @@ PRIMARY KEY(dept_no)
 );
 
 CREATE TABLE dept_emp (
-emp_no VARCHAR (10), 
+emp_no INT, 
 dept_no VARCHAR(10),
 FOREIGN KEY(emp_no) REFERENCES employees (emp_no), 
 FOREIGN KEY(dept_no) REFERENCES departments (dept_no),
@@ -16,25 +16,25 @@ PRIMARY KEY (emp_no, dept_no)
 	
 CREATE TABLE dept_manager (
 dept_no VARCHAR (10), 
-emp_no VARCHAR(10),
+emp_no INT,
 FOREIGN KEY(emp_no) REFERENCES employees (emp_no), 
 FOREIGN KEY(dept_no) REFERENCES departments (dept_no),
 PRIMARY KEY (emp_no, dept_no)
 );	
 	
 CREATE TABLE employees (
-emp_no VARCHAR(10), 
+emp_no INT, 
 emp_title_id VARCHAR(30), 
-birth_date VARCHAR(10), 
+birth_date DATE, 
 first_name VARCHAR(30), 
 last_name VARCHAR(30), 
-sex VARCHAR(10), 
-hire_date VARCHAR(10)
+sex VARCHAR(5), 
+hire_date DATE
 PRIMARY KEY (emp_no)
 );	
 
 CREATE TABLE salaries (
-emp_no VARCHAR(10), 
+emp_no INT, 
 salary INT,
 PRIMARY KEY (emp_no)
 );	
